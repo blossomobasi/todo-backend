276964c44b7425d4e87cf3d5ebabbceee85066f9
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-24 17:37:49 +0100

    Chore: Create a user route for creating all request

---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const userController = require("../controller/userController");
+
+const router = express.Router();
+
+router.route("/").get(userController.getAllUsers).post(userController.createUser);
+router.route("/:id").put(userController.updateUser).delete(userController.deleteUser);
+
+module.exports = router;
