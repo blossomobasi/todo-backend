70d5fddd8dc352ec58ca240c502c844f241e70f2
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-24 17:36:34 +0100

    Chore: Created a user schema and a model.

---
+++ model/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "First name is required."],
+        minlength: [3, "First name must be at least 3 characters."],
+        maxlength: [50, "First name must not be more than 50 characters."],
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name is required."],
+        minlength: [3, "Last name must be at least 3 characters."],
+        maxlength: [50, "Last name must not be more than 50 characters."],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required."],
+        unique: true,
+        match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, "Please provide a valid email."], // Regular expression for email validation
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required."],
+        min: [8, "Password must be at least 8 characters."],
+        select: false, // This will prevent the password from showing up in any output
+    },
+    role: {
+        type: String,
+        enum: ["user"],
+        default: "user",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
