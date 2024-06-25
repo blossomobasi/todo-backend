4225bbc51a902375597664baff7ed3254adbb0e3
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-25 19:37:07 +0100

    Update: Add getUser endpoint

--- controller/userController.js
+++ controller/userController.js
@@ -14,6 +14,19 @@ const getAllUsers = async (req, res) => {
     });
 };
 
+const getUser = catchAsync(async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+
+    if (!user) return next(new AppError("The user with the given ID was not found!", 404));
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            user,
+        },
+    });
+});
+
 const createUser = catchAsync(async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
 
@@ -43,7 +56,7 @@ const updateUser = catchAsync(async (req, res, next) => {
             runValidators: true,
         }
     );
-    if (!updatedUser) next(new AppError("The user with the given ID was not found!", 404));
+    if (!updatedUser) return next(new AppError("The user with the given ID was not found!", 404));
 
     res.status(200).json({
         status: "success",
@@ -56,7 +69,7 @@ const updateUser = catchAsync(async (req, res, next) => {
 const deleteUser = catchAsync(async (req, res, next) => {
     const user = await User.findByIdAndDelete(req.params.id);
 
-    if (!user) next(new AppError("The user with the given ID was not found!", 404));
+    if (!user) return next(new AppError("The user with the given ID was not found!", 404));
 
     res.status(204).json({
         status: "success",
@@ -64,4 +77,4 @@ const deleteUser = catchAsync(async (req, res, next) => {
     });
 });
 
-module.exports = { getAllUsers, createUser, updateUser, deleteUser };
+module.exports = { getAllUsers, getUser, createUser, updateUser, deleteUser };
