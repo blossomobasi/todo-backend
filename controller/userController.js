761ecb95f907a51626d4e2629f78356d633f46e8
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-26 07:52:30 +0100

    Refactor: Use catchAsync as a module export

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,6 @@
 const User = require("../model/userModel");
 const AppError = require("../utils/appError");
-const { catchAsync } = require("../utils/catchAsync");
+const catchAsync = require("../utils/catchAsync");
 
 const getAllUsers = async (req, res) => {
     const users = await User.find();
