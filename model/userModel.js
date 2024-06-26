7a651bfe5d10d48aa08106bdcc24a7932fad60cb
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-26 14:46:06 +0100

    Update: userModel to use username instead of firstName and lastName

--- model/userModel.js
+++ model/userModel.js
@@ -2,28 +2,24 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    firstName: {
+    username: {
         type: String,
-        required: [true, "First name is required."],
-        minlength: [3, "First name must be at least 3 characters."],
-        maxlength: [50, "First name must not be more than 50 characters."],
-    },
-    lastName: {
-        type: String,
-        required: [true, "Last name is required."],
-        minlength: [3, "Last name must be at least 3 characters."],
-        maxlength: [50, "Last name must not be more than 50 characters."],
+        required: [true, "username is required."],
+        unique: true,
+        minlength: [3, "username must be at least 3 characters."],
+        maxlength: [50, "username must not be more than 50 characters."],
+        trim: true,
     },
     email: {
         type: String,
-        required: [true, "Email is required."],
+        required: [true, "email is required."],
         unique: true,
         match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, "Please provide a valid email."], // Regular expression for email validation
     },
     password: {
         type: String,
-        required: [true, "Password is required."],
-        min: [8, "Password must be at least 8 characters."],
+        required: [true, "password is required."],
+        min: [8, "password must be at least 8 characters."],
         select: false, // This will prevent the password from showing up in any output
     },
     role: {
