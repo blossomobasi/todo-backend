191bd8ffadd875ef21dffa9c93d53d6059e69d1f
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-26 10:44:32 +0100

    Add authController file for handling signup, login and protect function

---
+++ controller/authController.js
@@ -0,0 +1,84 @@
+const { promisify } = require("util");
+const jwt = require("jsonwebtoken");
+const User = require("../model/userModel");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+
+function signToken(id) {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+}
+
+function createSendToken(res, statusCode, user) {
+    const token = signToken(user._id);
+
+    res.cookie("jwt", token, {
+        expiresIn: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        secure: process.env.NODE_ENV === "production",
+        httpOnly: true,
+    });
+
+    user.password = undefined;
+
+    res.status(statusCode).json({
+        status: "success",
+        token,
+        data: {
+            user,
+        },
+    });
+}
+
+const signup = catchAsync(async (req, res, next) => {
+    const { firstName, lastName, email, password } = req.body;
+
+    const newUser = await User.create({
+        firstName,
+        lastName,
+        email,
+        password,
+    });
+
+    createSendToken(res, 201, newUser);
+});
+
+const login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) return next(new AppError("Please Provide email and password!", 400));
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError("Incorrect email or password", 401));
+    }
+
+    createSendToken(res, 200, user);
+});
+
+const protect = catchAsync(async (req, res, next) => {
+    let token;
+
+    // Check if token exists
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) return next(new AppError("You are not logged in! Log in to get Access", 401)); // Unauthorized
+
+    // Verify the token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id);
+
+    if (!user)
+        return next(new AppError("The user belonging to the token does not longer exit!", 401));
+
+    req.user = user;
+
+    // GRANT ACCESS TO PROTECTED ROUTE
+    next();
+});
+
+module.exports = { signup, login, protect };
