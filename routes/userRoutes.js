48935f7126212eea75e929bb565d0bf4ca15910a
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-26 10:46:56 +0100

    Update: Add endpoint for login, signup, me

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const userController = require("../controller/userController");
+const authController = require("../controller/authController");
 
 const router = express.Router();
 
-router.route("/").get(userController.getAllUsers).post(userController.createUser);
+// Auth
+router.route("/me").get(authController.protect, userController.getMe);
+router.route("/signup").post(authController.signup);
+router.route("/login").post(authController.login);
+
+// Users
+router
+    .route("/")
+    .get(userController.getAllUsers)
+    .post(authController.protect, userController.createUser);
+
 router
     .route("/:id")
     .get(userController.getUser)
-    .put(userController.updateUser)
-    .delete(userController.deleteUser);
+    .put(authController.protect, userController.updateUser)
+    .delete(authController.protect, userController.deleteUser);
 
 module.exports = router;
