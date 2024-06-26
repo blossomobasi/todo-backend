d09aa7208e5e394ffd8607175fde73f04c5f141d
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-26 10:45:26 +0100

    Update error handler to handle non-operational error

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,3 +1,30 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (error) => {
+    const message = `Invalid ${error.path}: ${error.value}.`;
+
+    return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (error) => {
+    const message = `Duplicate field value: ${error.keyValue.email}. Please use another value!`;
+
+    return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (error) => {
+    const errors = Object.values(error.errors).map((el) => el.message);
+
+    const message = `Invalid input data. ${errors.join(". ")}`;
+
+    return new AppError(message, 400);
+};
+
+const handleJWTError = () => new AppError("Invalid token. Please log in again", 401);
+
+const handleJWTExpiredError = () =>
+    new AppError("Your token has expired! Please log in again", 401);
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -14,10 +41,11 @@ const sendErrorProd = (err, res) => {
             message: err.message,
         });
     } else {
-        console.error("ERROR 💥", err);
+        console.log("ERROR 💥", err);
+
         res.status(500).json({
             status: "error",
-            message: "Something went wrong!",
+            message: "Something went wrong",
         });
     }
 };
@@ -29,6 +57,14 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === "development") {
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === "production") {
+        let error = JSON.parse(JSON.stringify(err));
+
+        if (error.name === "CastError") error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+        if (error.name === "ValidationError") error = handleValidationErrorDB(error);
+        if (error.name === "JsonWebTokenError") error = handleJWTError();
+        if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+
         sendErrorProd(err, res);
     }
 };
