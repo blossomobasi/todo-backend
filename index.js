2f0294afe28e6e625b7dc86d32f2808b74cefc45
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-27 06:42:18 +0100

    Update: Use cors to allow origins

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const globalErrorHandler = require("./controller/errorController");
 
 const app = express();
@@ -9,6 +10,16 @@ app.use(express.json());
 if (process.env.NODE_ENV === "developmemt") {
     app.use(morgan("dev"));
 }
+
+const corsOptions = {
+    origin: "*",
+    // process.env.NODE_ENV === "production"
+    //     ? [process.env.FRONTEND_BASE_URL, "http://localhost:3000"]
+    //     : "*",
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 require("./routes")(app);
 
