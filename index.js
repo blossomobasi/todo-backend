a788d20a26ad26dc67ac8e89f4ac0103b1f2ea67
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-27 08:19:15 +0100

    Add cookie parser and include credentials to true in cors

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
 const globalErrorHandler = require("./controller/errorController");
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 if (process.env.NODE_ENV === "developmemt") {
     app.use(morgan("dev"));
 }
 
+app.use((req, res, next) => {
+    // console.log(req.cookies);
+
+    next();
+});
+
 const corsOptions = {
-    origin: "*",
-    // process.env.NODE_ENV === "production"
-    //     ? [process.env.FRONTEND_BASE_URL, "http://localhost:3000"]
-    //     : "*",
+    origin: process.env.FRONTEND_BASE_URL,
+    credentials: true,
     optionsSuccessStatus: 200,
 };
 
