eea85c69d31f390677b3c7d6e6499ac10547c46a
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-27 08:46:12 +0100

    Add middleware securities

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const helmet = require("helmet");
+const mongoSanitizer = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const morgan = require("morgan");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
@@ -7,12 +10,20 @@ const globalErrorHandler = require("./controller/errorController");
 
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
-
 if (process.env.NODE_ENV === "developmemt") {
     app.use(morgan("dev"));
 }
+
+// Middlewares
+app.use(helmet());
+app.use(express.json());
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitizer());
+
+// Data sanitization against XSS
+app.use(xss());
 
 app.use((req, res, next) => {
     // console.log(req.cookies);
@@ -20,6 +31,7 @@ app.use((req, res, next) => {
     next();
 });
 
+// CORS
 const corsOptions = {
     origin: process.env.FRONTEND_BASE_URL,
     credentials: true,
@@ -28,8 +40,10 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+// ROUTES
 require("./routes")(app);
 
+// GLOBAL ERROR MIDDLEWARE
 app.use(globalErrorHandler);
 
 module.exports = app;
